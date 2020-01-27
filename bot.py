987610494acb67b9e45c7f21a689fce2671863c2
Daniel Sivaldi Feres <danielferes@live.com>
2020-01-27 20:01:13 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ import telegram, logging
 import os
 
 token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
